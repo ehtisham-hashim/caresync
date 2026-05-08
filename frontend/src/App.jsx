cd9bdc2ee3adc240f96361cfec658d1f7fff8413
muhammadehtishamhashim <meh942007@gmail.com>
2026-05-08 11:18:26 +0500

    chore: update project dependencies and node_modules structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="bg-red-400 p-5 m-5 text-3xl rounded-2xl border-red-950 border-4 text-center">
+      Hello World
+    </h1>
+  );
+}
+
+export default App;
