f66734dd7c8bca6ef5b6cd61effc165f62f6ec1f
muhammadehtishamhashim <meh942007@gmail.com>
2026-05-12 19:04:41 +0500

    Fixed frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   return (
     <h1 className="bg-red-400 p-5 m-5 text-3xl rounded-2xl border-red-950 border-4 text-center">
-      Hello World
+      Hello World!!
     </h1>
   );
 }
